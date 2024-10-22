6acafefefca6c84833bed2253839ba1fc52a9717
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-22 20:15:48 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,4 +2,9 @@ import streamlit as st
 st.title("Hello world my friend")
 
 st.text("Hello GeeksForGeeks!!!")
-st.markdown("# This is a markdown")
+
+st.success("Success") 
+st.info("Information") 
+st.warning("Warning") 
+st.error("Error") 
+st.exception(exp)
