f0c9089f58739558c48886019c38c3339987a096
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-22 20:22:46 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,14 +1,5 @@
 import streamlit as st
 st.title("Hello world my friend")
 
-st.text("Hello GeeksForGeeks!!!")
-
-st.success("Success") 
-st.info("Information") 
-st.warning("Warning") 
-st.error("Error") 
-exp = ZeroDivisionError("Trying to divide by Zero")
-st.exception(exp)
-
-st.write("Text with write") 
-st.write(range(10))
+from PIL import Image img = Image.open("streamlit.png") 
+st.image(img, width=200)
