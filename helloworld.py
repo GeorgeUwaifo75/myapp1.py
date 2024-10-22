bfaa239b163c4f85ecb97bae88ed7bca57b1ab0e
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-22 20:32:18 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,3 +4,10 @@ st.title("Hello world my friend")
 if st.checkbox("Show/Hide"): 
    st.text("Showing the widget")
 
+status = st.radio("Select Gender: ", ('Male', 'Female')) 
+# conditional statement to print # Male if male is selected else print female # show the result using the success function 
+if (status == 'Male'): 
+   st.success("Male") 
+else: 
+   st.success("Female")
+
