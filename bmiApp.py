08011b787e66c1b96361889ce8c14eb219eb3d63
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-22 21:43:38 +0100

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -3,3 +3,19 @@ import streamlit as st
 st.title("BMI Calculation Master")
 weight = st.number_input("Enter your weight (in kgs)")
 status = st.radio('Select your height format: ', ('cms', 'meters', 'feet'))
+
+if(status == 'cms'): 
+# take height input in centimeters 
+  height = st.number_input('Centimeters') 
+  try: bmi = weight / ((height/100)**2) 
+  except: st.text("Enter some value of height") 
+elif(status == 'meters'): 
+# take height input in meters 
+  height = st.number_input('Meters') 
+  try: bmi = weight / (height ** 2) 
+  except: st.text("Enter some value of height") 
+else: # take height input in feet 
+  height = st.number_input('Feet') 
+# 1 meter = 3.28 
+  try: bmi = weight / (((height/3.28))**2) 
+  except: st.text("Enter some value of height") 
