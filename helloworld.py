3964028df458b328946c7a31b42352f351806ae5
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-22 20:23:42 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.title("Hello world my friend")
 
-from PIL import Image img = Image.open("streamlit.png") 
+from PIL import Image 
+img = Image.open("streamlit.png") 
 st.image(img, width=200)
