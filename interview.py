1c9c0c283f40c5b785786975ef0f873d667b4d25
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-23 23:59:20 +0100

    Update interview.py

--- interview.py
+++ interview.py
@@ -5,11 +5,12 @@ import time
 st.set_page_config()
 
 ph = st.empty()
-N = 5*60
+N = 3*60
 for secs in range(N,0,-1):
     mm, ss = secs//60, secs%60
     ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
-    time.sleep(1)
+    if secs == 10:
+        time.sleep(1)
 
 st.title("Interview Session")
 st.text("This is a set of questions for this interview session")
