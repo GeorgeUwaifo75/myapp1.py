f049770c14b9bf8006ef250763b88468d201e029
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-24 00:17:07 +0100

    Update interview.py

--- interview.py
+++ interview.py
@@ -5,7 +5,7 @@ import time
 st.set_page_config()
 
 ph = st.empty()
-N = 0.5*60
+N = 1*60
 for secs in range(N,0,-1):
     mm, ss = secs//60, secs%60
     ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
