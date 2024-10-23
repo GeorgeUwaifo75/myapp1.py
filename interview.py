bcc3ef4521df26d4cbb0eb9bf82cd768908bff34
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-23 23:08:30 +0100

    Update interview.py

--- interview.py
+++ interview.py
@@ -25,9 +25,19 @@ hobby = st.selectbox("Education: ", ['Secondary', 'Tertiery Bsc', 'Tertiery MSc'
 st.markdown("### Quick Qizz Session")
 quest1 = st.selectbox("How many states in Nigeria? ", ['30', '25', '36', '46', 'Others']) 
 quest2 = st.selectbox("How many planets in our solar system? ", ['5', '9', '7', '8', 'Others']) 
-quest3 = st.text_input("What year did Nigeria gain independence?", "")
+quest3 = st.number_input("What year did Nigeria gain independence?", "19xx")
+quest4 = st.text_input("What is  12 + 5 - 2 (3+5) ?", "0")
 
+counter=0
 if(st.button("Submit")): 
-     st.text("Thanks for participating!!!")
+     if quest1 == "36":
+          counter+=1
+     elif quest2 == "8": 
+          counter+=1
+     elif quest3 == 1960: 
+          counter+=1 
+     elif quest4 == "1": 
+          counter+=1        
+     st.text("Thanks for participating. You got ", counter, " of 4 correctly.")
 
 
