77416808999f97e48c068a74a6d459bd2fd6f9c5
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-24 00:15:07 +0100

    Update interview.py

--- interview.py
+++ interview.py
@@ -2,15 +2,15 @@ import streamlit as st
 
 import time
 
-st.set_page_config()
+#st.set_page_config()
 
-#ph = st.empty()
-N = 5*60
+ph = st.empty()
+N = 0.5*60
 for secs in range(N,0,-1):
     mm, ss = secs//60, secs%60
     ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
     time.sleep(1)
-ph = st.empty()
+st.set_page_config()
 
 st.title("Interview Session")
 st.text("This is a set of questions for this interview session")
