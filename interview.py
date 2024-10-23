b62e86433dfee74660790f4b404dd2328ba486d3
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-23 23:25:06 +0100

    Update interview.py

--- interview.py
+++ interview.py
@@ -22,20 +22,24 @@ hobby = st.selectbox("Education: ", ['Secondary', 'Tertiery Bsc', 'Tertiery MSc'
 
 st.markdown("### Quick Qizz Session")
 quest1 = st.selectbox("How many states in Nigeria? ", ['30', '25', '36', '46', 'Others']) 
+q1 = int(quest1)
 quest2 = st.selectbox("How many planets in our solar system? ", ['5', '9', '7', '8', 'Others']) 
+q2 = int(quest2)
 quest3 = st.number_input("What year did Nigeria gain independence?")
+q3 = int(quest3)
 quest4 = st.text_input("What is  12 + 5 - 2 (3+5) ?", "0")
 
 counter=0
 if(st.button("Submit")): 
-     if quest1 == "36":
+     if quest1 == q1:
           counter+=1
-     elif quest2 == "8": 
+     if quest2 == q2: 
           counter+=1
-     elif quest3 == 1960: 
+     if quest3 == q3: 
           counter+=1 
-     elif quest4 == "1": 
-          counter+=1        
-     st.write("Thanks for participating. You got ", counter, " of 4 correctly.")
+     if quest4 == "1": 
+          counter+=1 
+    perct = ((counter/4) * 100)     
+    st.write("Thanks for participating. You got ", counter, " of 4 correctly, which is ",perct, "%")
 
 
