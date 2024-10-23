19fbdd505273cd654c413c90b33fc8fee08dc522
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-23 23:12:29 +0100

    Update interview.py

--- interview.py
+++ interview.py
@@ -3,11 +3,9 @@ import streamlit as st
 st.title("Interview Session")
 st.text("This is a set of questions for this interview session")
 fname = st.text_input("What is your first name?", "Enter name")
-if fname:
-     st.write("Your first name is ", fname)
+
 lname = st.text_input("What is your last name?", "Enter name")
-if lname:
-     st.write("Your last name is ", lname)
+
 status = st.radio("Select Gender: ", ('Male', 'Female')) 
 
 # conditional statement to print 
