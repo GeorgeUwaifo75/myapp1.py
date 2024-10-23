b07ae6cf6a69f2204c9d147d1ec42b2f319eddf6
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-23 22:33:13 +0100

    Update interview.py

--- interview.py
+++ interview.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Interview Session")
 st.text("This is a set of questions for this interview session")
-fname = st.text_input("What is your first name", "Enter your first name...")
-if fname:
+fname = st.text_input("What is your first name?", "Enter your first name...")
+if fname !="":
   st.text("Your first name is ", fname)
