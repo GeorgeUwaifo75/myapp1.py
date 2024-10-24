084b88681cf79d457ff95312aad6f059b24e6e9d
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-24 14:18:50 +0100

    Create timerApp.py

---
+++ timerApp.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import timedef count_down(ts):
+    with st.empty():
+        while ts:
+            mins, secs = divmod(ts, 60)
+            time_now = '{:02d}:{:02d}'.format(mins, secs)
+            st.header(f"{time_now}")
+            time.sleep(1)
+            ts -= 1st.write("Time Up!")def main():
+    st.title("Pomodoro")
+    time_minutes = st.number_input('Enter the time in minutes ', min_value=1, value=25)
+    time_in_seconds = time_minutes * 60if st.button("START"):
+        count_down(int(time_in_seconds))if __name__ == '__main__':
+    main()
