dbbdae3a95756c4ef6add086fbf341e3b623f17b
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-24 15:08:44 +0100

    Update timerApp.py

--- timerApp.py
+++ timerApp.py
@@ -11,16 +11,16 @@ def count_down(ts):
             ts -= 1
         st.write("Time Up!")
 
-def form_body():
-    st.title("Pomodoro")
-    fname = st.text_input('Enter first name ', '')
-    lname = st.text_input('Enter last name ', '')
+
     
 def main():
-   # st.title("Pomodoro")
+   st.title("Pomodoro")
    time_minutes = st.number_input('Enter the time in minutes ', min_value=1, value=25)
-   form_body()
+   
+   fname = st.text_input('Enter first name ', '')
+   lname = st.text_input('Enter last name ', '')
    time_in_seconds = time_minutes * 60
+   
    count_down(int(120))
    # if st.button("START"):
    #      count_down(int(time_in_seconds))
