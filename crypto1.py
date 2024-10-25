d964ec0e37264b8b5a3f2d873882c9b40387be0a
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-25 14:17:22 +0100

    Update crypto1.py

--- crypto1.py
+++ crypto1.py
@@ -2,4 +2,18 @@ import streamlit as st
 
 
 st.header("My crypto hub")
-st.title("My Crypto smart buddy")
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+import numpy as np
+from sklearn import linear_model
+
+dataFile = 'DOGE.csv'
+df = pd.read_csv(dataFile)
+
+# Remove missing values
+df.dropna(inplace=True)
+
+print(df.info())
+print(df.describe())
+print(df.head(5))
