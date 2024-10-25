43d8ccfcc0dbce3e4d4bcd5e6cdca26789abefd2
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-25 14:19:37 +0100

    Update crypto1.py

--- crypto1.py
+++ crypto1.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.header("My crypto hub")
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
 from sklearn import linear_model
