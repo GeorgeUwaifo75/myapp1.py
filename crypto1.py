62a3f7352db749325ff03e69e06de1a86ee822a7
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-25 19:08:41 +0100

    Update crypto1.py

--- crypto1.py
+++ crypto1.py
@@ -1,10 +1,21 @@
 import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 import numpy as np
+from sklearn import linear_model
 
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
+st.header("Crypto Analytics Buddy")
+n = int(input("Enter 1-Doge, 2-Shib, 3-LTC,4-SOL, 5-XMR : "))
 
-ax.hist(arr, bins=20)
-
-st.pyplot(fig)
+# dataFile = 'DOGE.csv'
+if n==1:
+    dataFile = 'Binance_DOGEUSDT_1h.csv'
+elif n==2:   
+    dataFile = 'Binance_SHIBUSDT_1h.csv'
+elif n==3:   
+    dataFile = 'Binance_LTCUSDT_1h.csv'
+elif n==4:   
+    dataFile = 'Binance_SOLUSDT_1h.csv'
+elif n==5:   
+    dataFile = 'Binance_XMRUSDT_1h.csv'    
