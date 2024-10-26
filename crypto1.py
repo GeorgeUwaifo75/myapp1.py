84e60365637c01bf9717f1464fd908c128e7933e
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-26 14:30:02 +0100

    Update crypto1.py

--- crypto1.py
+++ crypto1.py
@@ -8,7 +8,7 @@ from sklearn import linear_model
 st.header("Crypto Analytics Buddy")
 n=1
 try:
-    n = int(st.text_input("Enter 1-Doge, 2-Shib, 3-LTC,4-SOL, 5-XMR : "))
+    n = int(st.text_input("Enter 1-Doge, 2-Shib, 3-LTC, 4-SOL, 5-XMR : "))
 except:
     st.write("Enter correct value.")
 
